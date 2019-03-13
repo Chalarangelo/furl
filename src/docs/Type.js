f00044db3aa8de9aa99499a3cd2494bb283031be
Angelos Chalaris <chalarangelo@gmail.com>
2019-03-13 12:17:10 +0200

    Docs boilerplate

---
+++ src/docs/Type.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Type = props => (
+  <div>
+    <h1>
+    Typography <small>Yada yada yada</small>
+    </h1>
+    <p>Using a typographic scale and great fonts can help communicate content more efficiently, while making designing easier.</p>
+    <h2>Variables</h2>
+    <h2>Loading fonts</h2>
+    <h2>Type Scale</h2>
+  </div>
+);
+
+export default Type;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+import Type from "./docs/Type";
+
+import Button from "./components/Button";
+import Card from "./components/Card";
+
+import "./styles/index.scss";
+
+ReactDOM.render(<div>
+  <Type></Type>
+</div>, document.getElementById("root"));
