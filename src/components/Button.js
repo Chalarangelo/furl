fd6c7478c9ca615242f75ae395952f558f4386a2
Angelos Chalaris <chalarangelo@gmail.com>
2019-03-14 20:39:15 +0200

    Button component
    Partially complete, missing a few things

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-const Button = props => <button>{props.label}</button>;
+const Button = ({
+  fill = 'solid', 
+  text ='normal', 
+  size = 'normal', 
+  color='plain', 
+  shape='normal',
+  type='button', 
+  disabled = false,
+  onClick,
+  children}) => {
+  let classNames = [];
+  classNames.push(fill);
+  if (text !== 'normal') classNames.push(text);
+  if (size !== 'normal') classNames.push(size);
+  if (color !== 'plain') classNames.push(color);
+  if (shape !== 'normal') classNames.push(shape);
+  return (
+    <button className={classNames.join(' ')}>
+      {children}
+    </button>
+  )
+};
 
 export default Button;
--- src/stories/index.js
+++ src/stories/index.js
@@ -13,7 +13,7 @@ storiesOf('Hyperlink', module)
     </Hyperlink>
   )
 )
-  .add("new tab", () => (
+  .add("blank", () => (
     <Hyperlink href="https://google.com" openIn="_blank">
       Open in new tab.
     </Hyperlink>
@@ -24,13 +24,137 @@ storiesOf('Hyperlink', module)
       Open in new tab with noopener noreferrer.
     </Hyperlink>
   )
-);
+)
+;
 
 storiesOf('Button', module)
-  .add("with text", () => (
-    <Button label={`Continue`} />
+  .add("default", () => (
+    <Button>
+      Continue
+    </Button>
   )
-);
+)
+  .add("large", () => (
+    <Button size='large'>
+      Continue
+    </Button>
+  )
+)
+  .add("small", () => (
+    <Button size='small'>
+      Continue
+    </Button>
+  )
+)
+  .add("rounded", () => (
+    <Button shape='rounded'>
+      Continue
+    </Button>
+  )
+)
+  .add("circle", () => (
+    <Button shape='circle'>
+      >.
+    </Button>
+  )
+)
+  .add("gradient", () => (
+    <Button fill='gradient'>
+      Continue
+    </Button>
+  )
+)
+  .add("outline", () => (
+    <Button fill='outline'>
+      Continue
+    </Button>
+  )
+)
+  .add("ghost", () => (
+    <Button fill='ghost'>
+      Continue
+    </Button>
+  )
+)
+  .add("uppercase", () => (
+    <Button text='uppercase'>
+      Continue
+    </Button>
+  )
+)
+  .add("danger", () => (
+    <Button color='danger'>
+      Continue
+    </Button>
+  )
+)
+  .add("danger gradient", () => (
+    <Button color='danger' fill='gradient'>
+      Continue
+    </Button>
+  )
+)
+  .add("danger outline", () => (
+    <Button color='danger' fill='outline'>
+      Continue
+    </Button>
+  )
+)
+  .add("danger ghost", () => (
+    <Button color='danger' fill='ghost'>
+      Continue
+    </Button>
+  )
+)
+  .add("success", () => (
+    <Button color='success'>
+      Continue
+    </Button>
+  )
+)
+  .add("success gradient", () => (
+    <Button color='success' fill='gradient'>
+      Continue
+    </Button>
+  )
+)
+  .add("success outline", () => (
+    <Button color='success' fill='outline'>
+      Continue
+    </Button>
+  )
+)
+  .add("success ghost", () => (
+    <Button color='success' fill='ghost'>
+      Continue
+    </Button>
+  )
+)
+  .add("warning", () => (
+    <Button color='warning'>
+      Continue
+    </Button>
+  )
+)
+  .add("warning gradient", () => (
+    <Button color='warning' fill='gradient'>
+      Continue
+    </Button>
+  )
+)
+  .add("warning outline", () => (
+    <Button color='warning' fill='outline'>
+      Continue
+    </Button>
+  )
+)
+  .add("warning ghost", () => (
+    <Button color='warning' fill='ghost'>
+      Continue
+    </Button>
+  )
+)
+;
 storiesOf('Card', module)
   .add("with text", () => (
     <Card>Hello</Card>
