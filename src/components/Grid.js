f0b4c050dfdce092134d22e3aa9f492ddb8c91c0
Angelos Chalaris <chalarangelo@gmail.com>
2019-03-18 12:38:06 +0200

    Add grid customization

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,10 +2,10 @@ import React from "react";
 import Row from "./Row";
 import Column from "./Column";
 
-const Grid = ({ children }) => {
+const Grid = ({ justify='start', align='start', children }) => {
   if (!Array.isArray(children)) children = [children];
   const rows = children.filter(item => Row.name == item.type.name);
-  return (<div className='grid-container'>{rows}</div>);
+  return (<div className={['grid-container', `align-${align}`, `${justify}-justify`].join(' ')} >{rows}</div>);
 }
 
 export { Grid , Row, Column };
