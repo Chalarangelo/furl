0bf72d25ce7a1020d58ab7d0b4a1fc75b1380d62
Angelos Chalaris <chalarangelo@gmail.com>
2019-03-26 14:31:11 +0200

    Update Dropdown chevron and add uppercase utility

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -12,7 +12,11 @@ const Dropdown = ({
   return (
     <details className={classNames.join(' ').trim()} id={id !== undefined ? id : false} open={isOpen}>
       <summary>{text}
+      {reverse ? 
+      <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 15l-6-6M12 9l-6 6" stroke="currentColor" strokeWidth="2.5" stroke-Linecap="round" strokeLinejoin="round"/></svg>
+      :
       <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 9l-6 6M12 15L6 9" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" /></svg>
+      }
       </summary>
       <div className="dropdown-content">
         {children}
