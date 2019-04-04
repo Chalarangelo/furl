2a8c6c467e95ce0e6823405e720ba8887b2a1d04
Angelos Chalaris <chalarangelo@gmail.com>
2019-04-04 13:20:27 +0300

    Fix Tooltip className

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -9,7 +9,7 @@ const Tooltip =({
 }) => {
   const [show, setShow] = React.useState(false);
   return (
-    <span className='tooltip' onMouseLeave={() => setShow(false)}>
+    <span className={[className, "tooltip"].join(' ').trim()} onMouseLeave={() => setShow(false)}>
       {show && <div className={`tooltip-bubble tooltip-${position}`}>
         <div className='tooltip-message'>{content}</div>
       </div>}
