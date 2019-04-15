9e19b9b57c88ed1d572a5aae77ad6e5584a318bd
Angelos Chalaris <chalarangelo@gmail.com>
2019-04-15 11:59:32 +0300

    Update Icon in Layout.Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Icon from "./Icon";
 
 const Sidebar = ({ id, className, outline ='none', children }) => {
   return (<aside
@@ -9,13 +10,13 @@ const SidebarTab = ({open, onClick}) =>
   open 
   ? <React.Fragment>
       <button className="layout-sidebar-tab" onClick={ onClick }>
-        < svg width = "24" height = "24" fill = "none" xmlns = "http://www.w3.org/2000/svg" > <path d="M18 6L6 18M18 18L6 6" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" /></svg>
+        <Icon name="x"/>
       </button>
       <div class="layout-sidebar-overlay"></div>
     </React.Fragment>
   : 
   <button className="layout-sidebar-tab" onClick={onClick}>
-    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 12H3M21 6H3M21 18H3" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokLinejoin="round" /></svg>
+        <Icon name="menu"/>
   </button>
 ;
 
