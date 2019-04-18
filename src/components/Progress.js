5de4371604581b699c16b5db001928ac61e5595a
Angelos Chalaris <chalarangelo@gmail.com>
2019-04-18 15:03:03 +0300

    Finalize Progress size variants

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -4,12 +4,13 @@ const Progress = ({
   value = 0,
   max = 100,
   color = 'normal',
+  size = 'normal',
   id, 
   className
 }) => (
   <progress 
     value={value} max={max} id={id !== undefined ? id : false} 
-    className={[color !== 'normal' ? color : '', className].join(' ').trim()}
+    className={[color !== 'normal' ? color : '', size !== 'normal' ? sizes : '', className].join(' ').trim()}
   >
     {`${+value/+max * 100}%`}
   </progress>
