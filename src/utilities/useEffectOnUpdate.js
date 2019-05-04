364ef54efcdb8e682bbdd82cc37f0da63d7b9736
Angelos Chalaris <chalarangelo@gmail.com>
2019-05-04 13:16:34 +0300

    Add useEffectOnUpdate
    Source: https://stackoverflow.com/a/53254028/1650200

---
+++ src/utilities/useEffectOnUpdate.js
@@ -0,0 +1,16 @@
+import React, { useLayoutEffect , useRef } from 'react';
+
+function useEffectOnUpdate(fn, params) {
+  const firstUpdate = useRef(true);
+
+  // Only run if this is not the first update.
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    fn();
+  }, params);
+}
+
+export default useEffectOnUpdate;
