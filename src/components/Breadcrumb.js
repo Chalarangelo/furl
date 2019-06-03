09e931382def661c122bd91e267625900744c112
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-03 20:52:12 +0300

    Resolve #28 - Breadcrumb bugs

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { normalizeChildren } from '../utilities/utils';
+import { normalizeChildren, generateUniqueId } from '../utilities/utils';
 
 const BreadcrumbItem = ({ id, className, children, ...rest }) => (
   <span id={id} className={['breadcrumb-item', className].join(' ').trim()} {...rest}>
@@ -14,11 +14,12 @@ const Breadcrumb = ({
   children,
   ...rest
 }) => {
+  const [breadcrumbsId, setBreadcrumbsId] = React.useState(generateUniqueId('breadcrumb'));
   children = normalizeChildren(children);
   let breadcrumbs = children.filter(item => BreadcrumbItem.name === item.type.name);
   breadcrumbs = breadcrumbs.reduce((acc, v, i) => {
     acc.push(v);
-    if (i !== breadcrumbs.length - 1) acc.push(<span className='breadcrumb-separator'>{separator}</span>);
+    if (i !== breadcrumbs.length - 1) acc.push(<span className='breadcrumb-separator' key={`${breadcrumbsId}-${i}`}>{separator}</span>);
     return acc;
   }, []);
   let classNames = [className, 'breadcrumb'];
