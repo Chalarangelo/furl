67d23e4e3524ee2a382c3d1f28f9b5a18196a42f
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-04 11:11:18 +0300

    Fix MenuItem accessibility issues
    #22

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,12 +13,12 @@ const Sidebar = ({ id, className, outline = 'none', children, ...rest }) => {
 const SidebarTab = ({ open, onClick, ...rest }) =>
   open
     ? <>
-      <button className='layout-sidebar-tab' onClick={onClick} {...rest}>
+      <button className='layout-sidebar-tab' onClick={onClick} title='Hide sidebar' {...rest}>
         <Icon name='x' />
       </button>
       <div className='layout-sidebar-overlay' />
     </>
-    : <button className='layout-sidebar-tab' onClick={onClick} {...rest}>
+    : <button className='layout-sidebar-tab' onClick={onClick} title='Show sidebar' {...rest}>
       <Icon name='menu' />
     </button>
 ;
