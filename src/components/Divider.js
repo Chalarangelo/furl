0daa823c9193c8a3457f4e9e362a69d308daa317
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-04 12:50:46 +0300

    Add separator role to vertical Divider
    #22

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Divider = ({ vertical = false, id, className, ...rest }) =>
   vertical ? (
-    <span id={id} className={['divider', className].join(' ').trim()} {...rest} />
+    <span id={id} className={['divider', className].join(' ').trim()} {...rest} role='separator'/>
   )
     : (
       <hr id={id} className={className} {...rest} />
