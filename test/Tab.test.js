58e01885afeaa9ae55a054c4926a9fb34d563dbc
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-11 11:25:30 +0300

    Add tests for Tab

---
+++ test/Tab.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { Tab, TabItem } from '../src/components';
+
+test('Content renders a <div> element of the appropriate class and with the appropriate structure.', () => {
+  const wrapper = mount(
+    <Tab>
+    </Tab>
+  );
+
+  expect(wrapper).toContainMatchingElement('div.tab-container');
+  expect(wrapper).toContainMatchingElement('div.tab-controls');
+  expect(wrapper).toContainMatchingElement('div.tab-content');
+});
+
+test('Content renders a <div> element with the given children.', () => {
+  const wrapper = mount(
+    <Tab openIndex={2}>
+      <TabItem title='Lorem Ipsum'><p>...</p></TabItem>
+      <TabItem title='Dolor Sit Amet'><p>...</p></TabItem>
+      <TabItem title='Peprendur'><p>...</p></TabItem>
+      <TabItem title='Nulla sit etiam'><p>...</p></TabItem>
+    </Tab>
+  );
+
+  expect(wrapper).toContainMatchingElement('div.tab-container');
+  expect(wrapper).toContainMatchingElements(4, 'div.tab-controls button');
+});
