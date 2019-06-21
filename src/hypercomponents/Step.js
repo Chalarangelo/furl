5be483f29af1fe8596de487d327fdb61033d81d0
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-21 19:01:22 +0300

    Update StepHOC
    Better definition

--- src/hypercomponents/Step.js
+++ src/hypercomponents/Step.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Step, StepItem } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined, hasKey, omitProps } from '../utilities/utils';
 
-const StepsHOC = (props) => {
-  if (!hasKey(props, 'data'))
-    return (
-      <Step {...props} />
+const StepsHOC = ({
+  data,
+  ...rest
+}) =>
+  isUndefined(data) ?
+    (<Step {...rest} />) :
+    (
+      <Step {...rest}>
+        {data.map((val, i) => {
+          console.log(typeof val);
+          let content = hasKey(val, 'content') ? val.content : val;
+          return (
+            <StepItem key={`i_${i}_${content}`} {...omitProps(val, ['content'])}>
+              {content}
+            </StepItem>
+          );
+        })}
+      </Step>
     );
-
-  let data = props.data;
-
-  return (
-    <Step {...omitProps(props, ['data'])}>
-      {data.map((val, i) => {
-        console.log(typeof val);
-        let content = hasKey(val, 'content') ? val.content : val;
-        return (
-          <StepItem key={`i_${i}_${content}`} {...omitProps(val, ['content'])}>
-            {content}
-          </StepItem>
-        );
-      })}
-    </Step>
-  );
-};
 
 export default StepsHOC;
