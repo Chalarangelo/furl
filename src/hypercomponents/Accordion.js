83babbeaf24e8488021a8cb700c738e631ac81b7
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-21 19:00:49 +0300

    Update AccordionHOC
    Better definition

--- src/hypercomponents/Accordion.js
+++ src/hypercomponents/Accordion.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Accordion, Collapse } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined } from '../utilities/utils';
 
-const AccordionHOC = (props) => {
-  if (!hasKey(props, 'data'))
-    return (
-      <Accordion {...props} />
+const AccordionHOC = ({
+  data,
+  ...rest
+}) => 
+  isUndefined(data) ? 
+    ( <Accordion {...rest} /> ) :
+    (
+      <Accordion {...rest}>
+        {data.map((val, i) => (
+          <Collapse title={val.title ? val.title : val.id ? val.id : `${i}`} key={`i_${i}_${val.title}`}>
+            {val.content}
+          </Collapse>
+        ))}
+      </Accordion>
     );
-
-  let data = props.data;
-
-  return (
-    <Accordion {...omitProps(props, ['data'])}>
-      {data.map((val, i) => (
-        <Collapse title={val.title ? val.title : val.id ? val.id : `${i}`} key={`i_${i}_${val.title}`}>
-          {val.content}
-        </Collapse>
-      ))}
-    </Accordion>
-  );
-};
 
 export default AccordionHOC;
