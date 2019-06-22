3aeeca8d7596509c9953e0ecd26fcf79e0a30fd4
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-22 10:14:09 +0300

    Update SelectInputHOC
    #9

--- src/hypercomponents/SelectInput.js
+++ src/hypercomponents/SelectInput.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Option, SelectInput } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined, hasKey, omitProps } from '../utilities/utils';
 
-const SelectInputHOC = (props) => {
-  if (!hasKey(props, 'data'))
-    return (
-      <SelectInput {...props} />
+const SelectInputHOC = ({
+  data,
+  ...rest
+}) => 
+  isUndefined(data) ?
+    (<SelectInput {...rest} />) :
+    (
+      <SelectInput {...rest}>
+        {data.map((val, i) => {
+          let content = hasKey(val, 'title') ? val.title : hasKey(val, 'value') ? val.value : val;
+          let value = hasKey(val, 'value') ? val.value : hasKey(val, 'title') ? val.title : val;
+          return (
+            <Option key={`i_${i}_${value}`} value={value} {...omitProps(val, ['title', 'value'])}>
+              {content}
+            </Option>
+          );
+        })}
+      </SelectInput>
     );
-
-  let data = props.data;
-
-  return (
-    <SelectInput {...omitProps(props, ['data'])}>
-      {data.map((val, i) => {
-        let content = hasKey(val, 'title') ? val.title : hasKey(val, 'value') ? val.value : val;
-        let value = hasKey(val, 'value') ? val.value : hasKey(val, 'title') ? val.title : val;
-        return (
-          <Option key={`i_${i}_${value}`} value={value} {...omitProps(val, ['title', 'value'])}>
-            {content}
-          </Option>
-        );
-      })}
-    </SelectInput>
-  );
-};
 
 export default SelectInputHOC;
