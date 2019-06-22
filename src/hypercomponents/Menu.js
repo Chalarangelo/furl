3f846050cbdb337d0b1ac8bf9097a99e91a06568
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-22 10:30:39 +0300

    Update MenuHOC
    #9

--- src/hypercomponents/Menu.js
+++ src/hypercomponents/Menu.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { Menu, MenuItem } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined, hasKey, omitProps } from '../utilities/utils';
 
-const MenuHOC = (props) => {
-  if (!hasKey(props, 'data'))
-    return (
-      <Menu {...props} />
+const MenuHOC = ({
+  data,
+  ...rest
+}) =>
+  isUndefined(data) ?
+    (<Menu {...rest} />) :
+    (
+      <Menu {...rest}>
+        {data.map((val, i) => {
+          let content = hasKey(val, 'title') ? val.title : val;
+          return (
+            <MenuItem key={`i_${i}_${content}`} {...omitProps(val, ['title'])}>
+              {content}
+            </MenuItem>
+          );
+        })}
+      </Menu>
     );
-
-  let data = props.data;
-
-  return (
-    <Menu {...omitProps(props, ['data'])}>
-      {data.map((val, i) => {
-        let content = hasKey(val, 'title') ? val.title : val;
-        return (
-          <MenuItem key={`i_${i}_${content}`} {...omitProps(val, ['title'])}>
-            {content}
-          </MenuItem>
-        );
-      })}
-    </Menu>
-  );
-};
 
 export default MenuHOC;
