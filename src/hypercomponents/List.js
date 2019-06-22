61df61b3366c818614eaa42ddc5669e0c1dcf21c
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-22 10:14:02 +0300

    Update ListHOC
    #9

--- src/hypercomponents/List.js
+++ src/hypercomponents/List.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import { List, ListItem } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined } from '../utilities/utils';
 
-const ListHOC = (props) => {
-  if(!hasKey(props, 'data')) 
-    return (
-      <List {...props} />
+const ListHOC = ({
+  data,
+  ...rest
+}) =>
+  isUndefined(data) ?
+    (<List {...rest} />) :
+    (
+      <List {...rest}>
+        {data.map((val, i) => (<ListItem key={`i_${i}_${val}`}>{val}</ListItem>))}
+      </List>
     );
-  
-  let data = props.data;
-
-  return (
-    <List {...omitProps(props, ['data'])}>
-      {data.map((val, i ) => (<ListItem key={`i_${i}_${val}`}>{val}</ListItem>))}
-    </List>
-  );
-};
 
 export default ListHOC;
