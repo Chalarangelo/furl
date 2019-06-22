992160d76a8fd822dfe718e765f9e7c5b66234a8
Angelos Chalaris <chalarangelo@gmail.com>
2019-06-22 10:13:52 +0300

    Update ComboboxInputHOC
    #9

--- src/hypercomponents/ComboboxInput.js
+++ src/hypercomponents/ComboboxInput.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Option, ComboboxInput } from '../components';
-import { hasKey, omitProps } from '../utilities/utils';
+import { isUndefined, hasKey, omitProps } from '../utilities/utils';
 
-const ComboboxInputHOC = (props) => {
-  if (!hasKey(props, 'data'))
-    return (
-      <ComboboxInput {...props} />
+const ComboboxInputHOC = ({
+  data,
+  ...rest
+}) =>
+  isUndefined(data) ?
+    (<ComboboxInput {...rest} />) :
+    (
+      <ComboboxInput {...rest}>
+        {data.map((val, i) => {
+          let content = hasKey(val, 'title') ? val.title : hasKey(val, 'value') ? val.value : val;
+          let value = hasKey(val, 'value') ? val.value : hasKey(val, 'title') ? val.title : val;
+          return (
+            <Option key={`i_${i}_${value}`} value={value} {...omitProps(val, ['title', 'value'])}>
+              {content}
+            </Option>
+          );
+        })}
+      </ComboboxInput>
     );
-
-  let data = props.data;
-
-  return (
-    <ComboboxInput {...omitProps(props, ['data'])}>
-      {data.map((val, i) => {
-        let content = hasKey(val, 'title') ? val.title : hasKey(val, 'value') ? val.value : val;
-        let value = hasKey(val, 'value') ? val.value : hasKey(val, 'title') ? val.title : val;
-        return (
-          <Option key={`i_${i}_${value}`} value={value} {...omitProps(val, ['title', 'value'])}>
-            {content}
-          </Option>
-        );
-      })}
-    </ComboboxInput>
-  );
-};
 
 export default ComboboxInputHOC;
