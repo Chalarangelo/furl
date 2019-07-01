f8fe47f1b572b01b5a85cdc4595bb262e27b488c
Angelos Chalaris <chalarangelo@gmail.com>
2019-07-01 10:29:14 +0300

    Update Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -15,7 +15,7 @@ const Search = ({
   ...rest
 }) =>
   withIcon ? (
-    <>
+    <div>
       <InputBase
         type='search' placeholder={placeholder}
         className={combineClassNames(['with-search-icon', className])} size={size} disabled={disabled}
@@ -27,9 +27,9 @@ const Search = ({
         <Icon name='search' width={16} height={16} />&zwnj;
       </span>
       {normalizeChildren(children).length ? <div className='search-result-box'>{children}</div> : ''}
-    </>
+    </div>
   ) : (
-    <>
+    <div>
       <InputBase
         type='search' placeholder={placeholder}
         className={className} size={size} disabled={disabled}
@@ -38,7 +38,7 @@ const Search = ({
         {...omitProps(rest)}
       />
       {normalizeChildren(children).length ? <div className='search-result-box'>{children}</div> : ''}
-    </>
+    </div>
   );
 
 export default Search;
